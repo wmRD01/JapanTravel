058a4169331376706adeaa7b197dac78c44301f6
wmRD01 <86639867+wmRD01@users.noreply.github.com>
2025-12-04 17:52:21 +0800

    Add files via upload

---
+++ data/trip-data.js
@@ -0,0 +1,418 @@
+// 日本東京富士行程與預設支出資料
+export const JP_TRIP_ID = 'trip_japan_2025_feb';
+
+export const JP_TRIP_DATA = [{
+        date: "02/17 (一)",
+        shortDate: "02/17",
+        fullDate: "2025-02-17",
+        title: "抵達日本・成田住宿",
+        flight: {
+            type: "arrival",
+            startTime: "12:50",
+            startAirport: "TPE",
+            number: "FLIGHT",
+            endTime: "16:50",
+            endAirport: "NRT",
+            arrivalOffset: 0
+        },
+        items: [{
+                time: "16:50",
+                type: "transport",
+                activity: "抵達日本",
+                location: "Narita International Airport",
+                note: "第二航廈"
+            },
+            {
+                time: "19:00",
+                type: "spot",
+                activity: "飯店 Check-in",
+                location: "HOTEL MYSTAYS Narita",
+                note: "熟悉周邊便利店超市"
+            }
+        ]
+    },
+    {
+        date: "02/18 (二)",
+        shortDate: "02/18",
+        fullDate: "2025-02-18",
+        title: "箱根・蘆之湖・溫泉",
+        items: [{
+                time: "10:00",
+                type: "transport",
+                activity: "新船橋取車",
+                location: "Funabashi",
+                note: "確認租車文件、導航"
+            },
+            {
+                time: "10:00",
+                type: "food",
+                activity: "海老名服務區",
+                location: "Ebina SA",
+                note: "休息、午餐"
+            },
+            {
+                time: "13:00",
+                type: "transport",
+                activity: "海賊觀光船",
+                location: "Ashinoko",
+                note: "遊蘆之湖，欣賞湖景"
+            },
+            {
+                time: "14:00",
+                type: "spot",
+                activity: "桃源台纜車",
+                location: "Togendai Station",
+                note: ""
+            },
+            {
+                time: "15:00",
+                type: "spot",
+                activity: "大涌谷",
+                location: "Owakudani",
+                note: "參觀、吃黑玉子"
+            },
+            {
+                time: "17:00",
+                type: "spot",
+                activity: "富士八景泡湯",
+                location: "Fuji Hakkei no Yu",
+                note: "Check-in、享受溫泉"
+            },
+            {
+                time: "19:00",
+                type: "food",
+                activity: "足炳服務區",
+                location: "Ashigara SA",
+                note: "晚餐、休息"
+            }
+        ]
+    },
+    {
+        date: "02/19 (三)",
+        shortDate: "02/19",
+        fullDate: "2025-02-19",
+        title: "Safari・山中湖・忍野八海",
+        items: [{
+                time: "07:00",
+                type: "spot",
+                activity: "蘆之湖鳥居拍照",
+                location: "Hakone Shrine",
+                note: "建議清晨"
+            },
+            {
+                time: "09:00",
+                type: "spot",
+                activity: "富士野生動物園",
+                location: "Fuji Safari Park",
+                note: "自駕或園內巴士"
+            },
+            {
+                time: "13:00",
+                type: "transport",
+                activity: "山中湖水陸兩用船",
+                location: "Yamanakako",
+                note: "留意班次"
+            },
+            {
+                time: "15:00",
+                type: "spot",
+                activity: "忍野八海",
+                location: "Oshino Hakkai",
+                note: "散步、蕎麥麵"
+            },
+            {
+                time: "18:00",
+                type: "spot",
+                activity: "紅富士之湯",
+                location: "Benifuji no Yu",
+                note: "泡湯、晚餐"
+            },
+            {
+                time: "20:00",
+                type: "spot",
+                activity: "前往休息站",
+                location: "Fujiyoshida Rest Area",
+                note: "車中泊準備"
+            }
+        ]
+    },
+    {
+        date: "02/20 (四)",
+        shortDate: "02/20",
+        fullDate: "2025-02-20",
+        title: "富士吉田・露營日",
+        items: [{
+                time: "06:00",
+                type: "spot",
+                activity: "富士吉田商店街",
+                location: "Fujiyoshida",
+                note: "清晨拍照"
+            },
+            {
+                time: "07:00",
+                type: "food",
+                activity: "羅森便利店",
+                location: "Lawson Fujikawaguchiko",
+                note: "拍照、買早餐"
+            },
+            {
+                time: "09:00",
+                type: "spot",
+                activity: "新倉山淺間公園",
+                location: "Chureito Pagoda",
+                note: "五重塔+富士山絕景"
+            },
+            {
+                time: "12:00",
+                type: "spot",
+                activity: "大池公園",
+                location: "Oike Park",
+                note: "散步遠眺"
+            },
+            {
+                time: "13:30",
+                type: "spot",
+                activity: "精進湖",
+                location: "Lake Shoji",
+                note: "富士山倒影"
+            },
+            {
+                time: "14:30",
+                type: "shop",
+                activity: "採買露營食材",
+                location: "Ogino",
+                note: "超市/道の駅"
+            },
+            {
+                time: "15:00",
+                type: "spot",
+                activity: "Fumotoppara 露營",
+                location: "Fumotoppara Campground",
+                note: "★務必15:00前進入，17:00後不可移動車輛"
+            }
+        ]
+    },
+    {
+        date: "02/21 (五)",
+        shortDate: "02/21",
+        fullDate: "2025-02-21",
+        title: "朝霧高原・煙火",
+        items: [{
+                time: "09:00",
+                type: "spot",
+                activity: "朝霧自然公園",
+                location: "Asagiri Kogen",
+                note: "散步拍照"
+            },
+            {
+                time: "10:30",
+                type: "spot",
+                activity: "Mochiya 兒童公園",
+                location: "Drive-in Mochiya",
+                note: ""
+            },
+            {
+                time: "13:00",
+                type: "spot",
+                activity: "西湖 療癒之里根場",
+                location: "Saiko Iyashi-no-Sato Nenba",
+                note: "古民家"
+            },
+            {
+                time: "15:00",
+                type: "spot",
+                activity: "大石公園",
+                location: "Oishi Park",
+                note: "河口湖景色"
+            },
+            {
+                time: "20:00",
+                type: "spot",
+                activity: "大池公園煙火",
+                location: "Oike Park",
+                note: "20:00-20:20"
+            },
+            {
+                time: "21:30",
+                type: "spot",
+                activity: "酒酒井休息站",
+                location: "Shisui PA",
+                note: "住宿休息"
+            }
+        ]
+    },
+    {
+        date: "02/22 (六)",
+        shortDate: "02/22",
+        fullDate: "2025-02-22",
+        title: "還車・個人行程 (船橋)",
+        items: [{
+                time: "09:00",
+                type: "transport",
+                activity: "船橋車站寄物",
+                location: "Funabashi Station",
+                note: "主團體本日回國，個人行程開始"
+            },
+            {
+                time: "09:30",
+                type: "transport",
+                activity: "辦理還車",
+                location: "Funabashi",
+                note: "10:00前完成"
+            },
+            {
+                time: "10:00",
+                type: "spot",
+                activity: "湯樂之里 泡湯",
+                location: "Funabashi Onsen Yura no Sato",
+                note: "放鬆休息"
+            },
+            {
+                time: "13:00",
+                type: "food",
+                activity: "船橋午餐",
+                location: "Funabashi Station",
+                note: ""
+            },
+            {
+                time: "14:30",
+                type: "shop",
+                activity: "百貨購物",
+                location: "Tobu Department Store Funabashi",
+                note: "約3小時"
+            },
+            {
+                time: "17:30",
+                type: "transport",
+                activity: "前往千葉",
+                location: "Chiba Station",
+                note: "JR總武線快速"
+            },
+            {
+                time: "18:00",
+                type: "spot",
+                activity: "飯店 Check-in",
+                location: "Chiba",
+                note: ""
+            },
+            {
+                time: "15:20",
+                type: "flight",
+                activity: "【主團體】飛機起飛",
+                location: "NRT",
+                note: "主團體返台"
+            }
+        ]
+    },
+    {
+        date: "02/23 (日)",
+        shortDate: "02/23",
+        fullDate: "2025-02-23",
+        title: "成田山・返國",
+        flight: {
+            type: "departure",
+            startTime: "15:30",
+            startAirport: "NRT",
+            number: "CX451",
+            endTime: "18:40",
+            endAirport: "TPE",
+            arrivalOffset: 0
+        },
+        items: [{
+                time: "07:30",
+                type: "transport",
+                activity: "前往成田站",
+                location: "Narita Station",
+                note: "行李寄放車站"
+            },
+            {
+                time: "08:30",
+                type: "spot",
+                activity: "成田山新勝寺",
+                location: "Naritasan Shinshoji Temple",
+                note: "先去川豐抽號碼牌"
+            },
+            {
+                time: "10:00",
+                type: "food",
+                activity: "川豐 鰻魚飯",
+                location: "Kawatoyo Honten",
+                note: "建議第一批入座"
+            },
+            {
+                time: "11:30",
+                type: "transport",
+                activity: "前往機場",
+                location: "Narita International Airport",
+                note: "提早2.5-3小時"
+            },
+            {
+                time: "19:40",
+                type: "transport",
+                activity: "機場捷運",
+                location: "Taoyuan Airport MRT",
+                note: "往高鐵站"
+            },
+            {
+                time: "20:21",
+                type: "transport",
+                activity: "高鐵南下",
+                location: "HSR Taoyuan Station",
+                note: "往台中"
+            },
+            {
+                time: "21:30",
+                type: "spot",
+                activity: "抵達台中",
+                location: "Taichung Station",
+                note: "平安賦歸"
+            }
+        ]
+    }
+];
+
+export const JP_EXPENSES = [{
+        item: 'Suica卡 (短期)',
+        amount: 1000,
+        payer: '我'
+    },
+    {
+        item: '露營場地與裝備租借',
+        amount: 52000,
+        payer: '我'
+    },
+    {
+        item: '紅富士之湯',
+        amount: 600,
+        payer: '我'
+    },
+    {
+        item: '船橋溫泉 (湯樂之里)',
+        amount: 1050,
+        payer: '我'
+    },
+    {
+        item: 'JR (船橋->千葉)',
+        amount: 240,
+        payer: '我'
+    },
+    {
+        item: 'JR (千葉->成田)',
+        amount: 510,
+        payer: '我'
+    },
+    {
+        item: '電車 (成田->機場)',
+        amount: 200,
+        payer: '我'
+    }
+];
+
+export const FirebaseConfig = {
+    apiKey: "AIzaSyA0CWPIeiD67vD554dQJ6S25d6C1u04zFU",
+    authDomain: "gojapen-63adb.firebaseapp.com",
+    projectId: "gojapen-63adb",
+    storageBucket: "gojapen-63adb.firebasestorage.app",
+    messagingSenderId: "118671479926",
+    appId: "1:118671479926:web:aea90b63264c28746b7b57"
+};
