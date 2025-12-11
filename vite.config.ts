5aa1098a850db8232bf7a9136cffa21a19c1d420
JaiJai <wm.rd01@gmail.com>
2025-12-11 16:35:00 +0800

    創立docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [vue()],
+    base: './',             // 若要相對路徑部署可加這行
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
@@ -13,5 +14,9 @@ export default defineConfig({
         __VUE_PROD_DEVTOOLS__: false,
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
     },
+    build: {
+        outDir: 'docs',  // 你希望的輸出資料夾
+        emptyOutDir: true     // 建議清空舊輸出
+    }
 });
 
